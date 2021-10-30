d6f985519276da300a6d8dd0354d5cd23bc7cd45
lemanh <lemanh@DESKTOP-SRM6FVE>
2021-10-30 15:28:52 +0700

    thêm kết nối

--- QLLTCB/KetNoi/KetNoi.cs
+++ QLLTCB/KetNoi/KetNoi.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,17 @@ namespace QLLTCB.KetNoi
 {
     class KetNoi
     {
+        string ConnectionString = "";
+        SqlConnection con;
+        public KetNoi() { }
+        //khởi tạo kết nối
+        public void Khoitaoketnoi() {
+            con = new SqlConnection(@"Data Source = (LocalDB)\MSSQLLocalDB; AttachDbFilename = D:\C#\QLLTCB\QLLTCB\Appdata\QLLichTrinhchuyenbay.mdf;Integrated Security=True");
+            con.Open();
+        }
+        //đóng kết nôi
+        public void DongketNoi() {
+            con.Close();
+        }
     }
 }
